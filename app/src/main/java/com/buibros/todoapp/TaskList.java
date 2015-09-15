70a5445fb76e8f835325a130cc04a3b26233a429
Kevin Bui <Kevinthuybui@gmail.com>
2015-09-14 19:59:58 -0700

    Removed Main Activity
    Added a blank activity with fragment called Task List

--- app/src/main/java/com/buibros/todoapp/TaskList.java
+++ app/src/main/java/com/buibros/todoapp/TaskList.java
@@ -1,22 +1,23 @@
 package com.buibros.todoapp;
 
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class TaskList extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_task_list);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_task_list, menu);
         return true;
     }
 
