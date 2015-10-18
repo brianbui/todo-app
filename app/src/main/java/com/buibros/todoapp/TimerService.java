8d51a63186a9f737c113c2fa47453d7dde98c2c9
Brian Bui <brian.bui.b@gmail.com>
2015-10-18 12:45:14 -0700

    Added timer service

---
+++ app/src/main/java/com/buibros/todoapp/TimerService.java
@@ -0,0 +1,27 @@
+package com.buibros.todoapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TimerService extends Service {
+    public TimerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+}
